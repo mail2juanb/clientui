f4a05f753548d34f6756e4ae65ccc10cafc0aa2f
juanb <mail2juanb@yahoo.fr>
2025-08-22 11:15:30 +0200

    - Add controller Home - Add GET list of Patients

---
+++ src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -0,0 +1,42 @@
+package com.clientui.clientui.controller;
+
+import com.clientui.clientui.beans.PatientBean;
+import com.clientui.clientui.proxies.MicroservicePatientsProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    private final MicroservicePatientsProxy patientsProxy;
+
+    public ClientController(MicroservicePatientsProxy patientsProxy) {
+        this.patientsProxy = patientsProxy;
+    }
+
+
+    @RequestMapping("/")
+    public String showHomes() {
+        return "home";
+    }
+
+    @RequestMapping("/patients")
+    public String showPatients(Model model) {
+        List<PatientBean> patients = patientsProxy.retrievePatientList();;
+        model.addAttribute("patients", patients);
+        return "list";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        final PatientBean patient = patientsProxy.retrievePatientId(id);
+        model.addAttribute("patient", patient);
+        return "update";
+    }
+
+    //TODO : Ajouter de nouveaux patients
+}
